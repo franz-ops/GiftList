0b352eb8f0faaccaa69c55bee7a680eb13aa125d
franz-ops <francesco.lobascio@gmail.com>
2024-10-06 17:40:30 +0200

    Added proof verification

--- client/index.js
+++ client/index.js
@@ -6,9 +6,16 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkletree = new MerkleTree(niceList)
+
+  // find the proof that norman block is in the list 
+  const name = 'Norman Block';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkletree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
